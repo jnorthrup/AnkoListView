dbc62ca2e6e2276cafed595dcd25da3507782a14
Jim Northrup <jnorthrup@users.noreply.github.com>
2017-10-06 15:13:01 +0700

    the inspector has suggested a few tweaks

--- app/src/main/java/com/xyz/BarberPoleList.kt
+++ app/src/main/java/com/xyz/BarberPoleList.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.ListView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
+import xyz.com.xyz.R
 import java.util.*
 
 
@@ -18,13 +19,13 @@ class BarberPoleList : Activity() {
         relativeLayout {
             val outOfOrderButton: Button = button {
                 id = Random().nextInt()//required for relativeLayout "above"
-                text = "push me"
+                text = context.getString(R.string.buttonText)
             }.lparams { alignParentBottom() }
             val lv = listView().lparams {
                 alignParentTop()
                 above(outOfOrderButton)
             }
-            val poleAd: PoleAdapter = PoleAdapter(lv)
+            val poleAd = PoleAdapter(lv)
             lv.adapter = poleAd
 
             outOfOrderButton.onClick {
@@ -34,28 +35,24 @@ class BarberPoleList : Activity() {
     }
 }
 
-class PoleAdapter(val listView: ListView) : BaseAdapter() {
-    val li = MutableList<Int>(10, { Random().nextInt(100) })
+class PoleAdapter(private val listView: ListView) : BaseAdapter() {
+    private val li = MutableList(10, { Random().nextInt(100) })
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        return with(listView.context) {
-            horizontalProgressBar {
-                id = java.util.Random().nextInt()
-                progress = getItem(p0) as Int
-                onClick {
-                    li[p0] = java.util.Random().nextInt(100)
-                    notifyDataSetChanged()
-                    invalidate()
-                }
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View = with(listView.context) {
+        horizontalProgressBar {
+            id = java.util.Random().nextInt()
+            progress = getItem(p0) as Int
+            onClick {
+                li[p0] = java.util.Random().nextInt(100)
+                notifyDataSetChanged()
+                invalidate()
             }
         }
     }
 
     override fun getItem(p0: Int): Any = li[p0]
 
-    override fun getItemId(p0: Int): Long {
-        return 0
-    }
+    override fun getItemId(p0: Int): Long = 0
 
     override fun getCount(): Int = li.size
     operator fun plusAssign(nextInt: Int) {
