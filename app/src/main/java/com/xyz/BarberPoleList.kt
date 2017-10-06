d74c5e3666d620c571acfe4d2174408ee78ff614
Jim Northrup <jnorthrup@users.noreply.github.com>
2017-10-06 15:04:55 +0700

    examining the ability to place a widget above a previously declared anko widget, which costs a lot of scoping upward.
    added a pushbutton and a growable adapter, and sorted out the details of getting the contexts sorted

--- app/src/main/java/com/xyz/BarberPoleList.kt
+++ app/src/main/java/com/xyz/BarberPoleList.kt
@@ -5,47 +5,63 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ListView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 import java.util.*
 
 
-/**
- * Created by jim on 10/5/2017.
- */
 class BarberPoleList : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        verticalLayout {
+        relativeLayout {
+            val outOfOrderButton: Button = button {
+                id = Random().nextInt()//required for relativeLayout "above"
+                text = "push me"
+            }.lparams { alignParentBottom() }
+            val lv = listView().lparams {
+                alignParentTop()
+                above(outOfOrderButton)
+            }
+            val poleAd: PoleAdapter = PoleAdapter(lv)
+            lv.adapter = poleAd
 
-            listView {
-                val li: MutableList<Int> = (0..10).map {
-                    Random().nextInt(100)
-                }.toMutableList()
-                adapter = object : BaseAdapter() {
-                    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-                        return with(this@listView.context) {
-                            horizontalProgressBar {
-                                id = Random().nextInt()
-                                progress = getItem(p0) as Int
-                                onClick {
-                                    li[p0] = Random().nextInt(100)
-                                    notifyDataSetChanged()
-                                    invalidate()
-                                }
-                            }
-                        }
-                    }
-
-                    override fun getItem(p0: Int): Any = li[p0]
-
-                    override fun getItemId(p0: Int): Long {
-                        return getView(p0, this@listView, this@listView).id.toLong()
-                    }
-
-                    override fun getCount(): Int = li.size
-                }
+            outOfOrderButton.onClick {
+                poleAd += java.util.Random().nextInt(100)
             }
         }
     }
 }
+
+class PoleAdapter(val listView: ListView) : BaseAdapter() {
+    val li = MutableList<Int>(10, { Random().nextInt(100) })
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        return with(listView.context) {
+            horizontalProgressBar {
+                id = java.util.Random().nextInt()
+                progress = getItem(p0) as Int
+                onClick {
+                    li[p0] = java.util.Random().nextInt(100)
+                    notifyDataSetChanged()
+                    invalidate()
+                }
+            }
+        }
+    }
+
+    override fun getItem(p0: Int): Any = li[p0]
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int = li.size
+    operator fun plusAssign(nextInt: Int) {
+        li += nextInt
+        notifyDataSetChanged()
+        listView.invalidate()
+    }
+}
+
